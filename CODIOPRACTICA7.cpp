0ce9b0dac676376335bf698e77981fb94d63c89f
AlexisElCapo <72107919+AlexisElCapo@users.noreply.github.com>
2020-11-23 15:51:05 -0600

    Add files via upload

---
+++ CODIOPRACTICA7.cpp
@@ -0,0 +1,22 @@
+// Seleccion de librerias
+#include<stdio.h>
+
+// Declaracion de variables globales
+int edadDeAlumnos;
+
+// Declaración de Funciones
+int main(){
+	// Declaracion de variables locales
+	char ALUMNO=20;
+	int edades;
+	float operacion=10;
+	// Bloque de INstrucciones
+	printf("Escribe la edad");
+	scanf("%i",&edades);
+	
+	operacion=edades+20;
+	printf(":\n %f", operacion);
+	
+	return 0;
+}
+
